2924130cf26212bc57df6485915feb12fddaa61c
Your Name <79922086+AV2606@users.noreply.github.com>
2022-09-05 11:54:25 +0300

    added fetch files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -6,17 +6,43 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testApp';
 
   constructor(private httpClient:HttpClient){
-    this.httpClient.get('assets/File.txt', {responseType: 'text'})
-        .subscribe(data => {
-          let f=document.getElementById('f');
-          if(f != null)
-          f.innerText=data;
-          console.log(f);
-        });
-
+   
   }
+  ngOnInit(): void {
+    let d=document.getElementById('f');
+    this.readTextFile('assets/File.txt').then((value)=>{
+      console.log(value);
+      console.log(d);
+      if(d!=null)
+      d.innerText=value as any});
+  }
+
+  async readTextFile(file:any):Promise<string | undefined> {
+    let r;
+    await fetch(file)
+        .then(async response => {
+            //console.log(response);
+            if (response.status === 200)
+                r = await response.text();
+        });
+    return r;
+}
+async GetNormalizedFiles(folder:any) {
+    let counter = 1;
+    let arr:any[] = [];
+    while (true) {
+        let f = await this.readTextFile(folder + `${counter}/text.txt`);
+        //console.log(f);
+        if (f != undefined && f != null)
+            arr.push(f);
+        else
+            break;
+        counter++
+    }
+    return arr;
+}
 }
